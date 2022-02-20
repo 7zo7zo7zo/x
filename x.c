a64cc997754bd3b3ad31706d3e9d891cbb025edd
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-02-19 22:14:57 -0800

    Update x.c

--- x.c
+++ x.c
@@ -6,9 +6,31 @@
 int main(int argc, char *argv[]) {
 
         Display *dpy;
+        int scr;
+        Window parent;
         Window win;
+        XEvent e;
 
         dpy = XOpenDisplay(NULL);
+        scr = DefaultScreen(dpy);
+        parent = XRootWindow(dpy, scr);
+        win = XCreateSimpleWindow(dpy, parent, 100, 100, 500, 500, 2, BlackPixel(dpy, scr), WhitePixel(dpy, scr));
 
+        GC gc = DefaultGC(dpy, scr);
+
+        XSelectInput(dpy, win, ExposureMask | KeyPressMask);
+        XMapWindow(dpy, win);
+        
+        while(1) {
+                XNextEvent(dpy, &e);
+                if(e.type == Expose) {
+                        XFillRectangle(dpy, win, gc, 120, 150, 50, 60);
+                }
+                if(e.type == KeyPress) {
+                        break;
+                }
+        }
+        
+        XCloseDisplay(dpy);
         return 0;
 }
